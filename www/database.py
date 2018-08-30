2d23c041ec8a6fe385a5578e5b2b364e07779eb5
ikrasowski <ikrasowski@danaide.com.ar>
2018-08-30 10:36:07 -0300

    IMPORTANTE: se modifica db_name de samples a tp2, y el binding del puerto, no anda si no

--- www/database.py
+++ www/database.py
@@ -11,8 +11,8 @@ class Database(object):
     db_user = os.getenv("DB_USER") if os.getenv("DB_USER") != None else "example"
     db_pass = os.getenv("DB_PASS") if os.getenv("DB_PASS") != None else "example"
     db_host = os.getenv("DB_HOST") if os.getenv("DB_HOST") != None else "db"
-    db_name = os.getenv("DB_NAME") if os.getenv("DB_NAME") != None else "samples"
-    db_port = os.getenv("DB_PORT") if os.getenv("DB_PORT") != None else "3309"
+    db_name = os.getenv("DB_NAME") if os.getenv("DB_NAME") != None else "tp2"
+    db_port = os.getenv("DB_PORT") if os.getenv("DB_PORT") != None else "3306"
     Base = declarative_base()
     
     def get_session(self):
