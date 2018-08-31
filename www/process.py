b4a036c2abdc4893ca39963b618520ac797ce839
ivankras <ivankras@hotmail.com>
2018-08-31 00:41:28 -0300

    Documentación del process.py

--- www/process.py
+++ www/process.py
@@ -9,8 +9,10 @@ import sys
 class GracefulKiller:
     kill_now = False
     def __init__(self):
+        # Comportamiento ante una señal de terminacion
         signal.signal(signal.SIGTERM, self.exit_gracefully)
 
+    # Finalizacion del proceso
     def exit_gracefully(self, signum, frame):
         self.kill_now = True
 
@@ -18,6 +20,8 @@ class GracefulKiller:
 def main(db):
     killer = GracefulKiller()
     session = db.get_session()
+
+    # Valores iniciales por defecto para las variables atmosfericas
     temperature = 22
     humidity = 10
     pressure = 1013
@@ -25,20 +29,24 @@ def main(db):
     while True:
         db.set_sample(temperature, humidity, pressure, windspeed)
 
+        # "Muestreo" de temperatura (entre -10°C y 35°C)
         temperature += random.randint(0,3) * random.choice([-1,0,1])
         if temperature < -10:
             temperature = -10
         elif temperature > 35:
             temperature = 35
         
+        # "Muestreo" de humedad (entre 0% y 100%)
         humidity += random.randint(0,10) * random.choice([-1,0,1])
         if humidity < 0:
             humidity = 0
         elif humidity > 100:
             humidity = 100
         
+        # "Muestreo" de presion atmosférica
         pressure += random.randrange(0,100,7) * random.choice([-1,0,1])
         
+        # "Muestreo" de velocidad del viento (entre 0 km/h y 300 km/h)
         windspeed += random.randint(0,5) * random.choice([-1,0,1])
         if windspeed < 0:
             windspeed = 0
